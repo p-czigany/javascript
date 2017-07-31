4738b4d7100e93ff5a51e872881ba32bdc914131
Peter Czigany <p.czigany@gmail.com>
2017-07-31 14:53:35 +0200

    Add variable types exercise

---
+++ helloworld/variable-types.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var name = "Peti";
+var age = 31;
+var height = 1.8;
+var married = false;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof height);
+console.log(typeof married);
