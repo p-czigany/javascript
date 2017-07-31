df4cad978d4ee159407236740cd87facaad0e487
Peter Czigany <p.czigany@gmail.com>
2017-08-01 01:25:27 +0200

    Remove comments

--- helloworld/bmi-counter.js
+++ helloworld/bmi-counter.js
@@ -3,5 +3,4 @@
 var massInKg = 81.2;
 var heightInM = 1.78;
 
-// Print the Body mass index (BMI) based on these values
-console.log(massInKg / Math.pow(heightInM, 2));
+console.log("BMI index: " + massInKg / Math.pow(heightInM, 2));
