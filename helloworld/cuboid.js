300e080fd61af9032a0b4a63b4c6cbc8bea4fc56
Peter Czigany <p.czigany@gmail.com>
2017-08-01 01:30:24 +0200

    Add cuboid counter exercise

---
+++ helloworld/cuboid.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+var a = 3;
+var b = 4;
+var c = 5;
+console.log("Surface Area: " + 2 * ((a * b) + (a * c) + (b * c)));
+console.log("Volume: " + (a * b * c));
