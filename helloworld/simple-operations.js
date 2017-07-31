e1dedc6793bbb1ff7b710dd61a06f0683397c616
Peter Czigany <p.czigany@gmail.com>
2017-07-31 14:34:55 +0200

    Add simple operations exercise

---
+++ helloworld/simple-operations.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var numbers = [22, 13];
+
+console.log(numbers[0] + numbers[1]);
+console.log(numbers[0] - numbers[1]);
+console.log(numbers[0] * numbers[1]);
+console.log(numbers[0] / numbers[1]);
+console.log(numbers[0] % numbers[1]);
