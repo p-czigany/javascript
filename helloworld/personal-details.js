9d58afd37d4afcc2b51f31f5d3084ab694e49ae7
Peter Czigany <p.czigany@gmail.com>
2017-07-31 14:24:43 +0200

    Add personal details printer

---
+++ helloworld/personal-details.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var name = "Peti";
+var age = 31;
+var height = 1.8;
+
+console.log(name);
+console.log(age);
+console.log(height);
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
