f9e0fa88e8d018a5fdc0c37ebeb2c766f9ec5f0d
Peter Czigany <p.czigany@gmail.com>
2017-07-31 14:09:13 +0200

    Remove comments

--- helloworld/three-helloes.js
+++ helloworld/three-helloes.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// Greet 3 of your classmates with this program, in three separate lines
-// like:
-//
-// Hello, Esther!
-// Hello, Mary!
-// Hello, Joe!
-
 var classmates = ["Marci", "Bálint", "Fanni... mindegy, a Lány"];
 
 for (var i = 0; i < 3; i++){
