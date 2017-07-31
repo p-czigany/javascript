2860f133f0927ec885122c0b26d33c6c16ac972b
Peter Czigany <p.czigany@gmail.com>
2017-07-31 14:25:27 +0200

    Polish

--- helloworld/personal-details.js
+++ helloworld/personal-details.js
@@ -7,13 +7,3 @@ var height = 1.8;
 console.log(name);
 console.log(age);
 console.log(height);
-// Write a program that prints a few details to the console about you
-// It should print each detail to a new line.
-//  - Your name
-//  - Your age
-//  - Your height in meters (as a decimal fraction)
-//
-//  Example output:
-//  John Doe
-//  31
-//  1.87
