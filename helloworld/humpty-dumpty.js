3996a966c28c406665d0b14821f5071d6b64ac97
Peter Czigany <p.czigany@gmail.com>
2017-07-31 14:17:32 +0200

    Add humpty-dumpty console exercise

---
+++ helloworld/humpty-dumpty.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var lines = [
+    'Humpty Dumpty sat on a wall,',
+    'Humpty Dumpty had a great fall.',
+    'All the king\'s horses and all the king\'s men',
+    'Couldn\'t put Humpty together again.'
+];
+
+for (var i = 0; i < 4 ; i++){
+    console.log(lines[i]);
+}
