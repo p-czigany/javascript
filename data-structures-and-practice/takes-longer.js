7fb7dc5ae5803822ff573a6ed57b76c98a23e610
Peter Czigany <p.czigany@gmail.com>
2017-08-09 13:13:08 +0200

    Add string insertion exercise

---
+++ data-structures-and-practice/takes-longer.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law."
+quote = quote.replace('It you', 'It always takes longer than');
+console.log(quote);
