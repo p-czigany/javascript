bdf6b99c992194763f79cd65ec0567192aa5b695
Peter Czigany <p.czigany@gmail.com>
2017-08-13 23:13:56 +0200

    Save elements to local variable

--- document-object-model/classlist-1.js
+++ document-object-model/classlist-1.js
@@ -1,18 +1,20 @@
+const paragraphs = document.getElementsByTagName('p');
+
 /**
  * Does the third p have a cat class?
  * If so, alert 'YEAH CAT!'
  */
-if (document.getElementsByTagName('p')[2].classList.contains('cat')) {
+if (paragraphs[2].classList.contains('cat')) {
   alert('YEAH CAT!');
 }
 
 // If the fourth p has a 'dolphin' class, replace apple's content with 'pear'
-if (document.getElementsByTagName('p')[3].classList.contains('dolphin')) {
+if (paragraphs[3].classList.contains('dolphin')) {
   document.getElementsByClassName('apple')[0].textContent = 'pear';
 }
 
 // If the first p has an 'apple' class, replace cat's content with 'dog'
-if (document.getElementsByTagName('p')[0].classList.contains('apple')) {
+if (paragraphs[0].classList.contains('apple')) {
   document.getElementsByClassName('cat')[0].textContent = 'dog';
 }
 
