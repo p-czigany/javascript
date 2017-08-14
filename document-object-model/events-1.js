7551a833b6f453557b17b5381d84349bbc2d6a6c
Peter Czigany <p.czigany@gmail.com>
2017-08-14 10:58:57 +0200

    Change function definition to named expression

--- document-object-model/events-1.js
+++ document-object-model/events-1.js
@@ -1,11 +1,11 @@
 // Turn the party on and off by clicking the button.
 const button = document.querySelector('button');
 const body = document.querySelector('body');
-function switchParty() {
+const switchParty = function switchPartyFunction() {
   if (body.classList.contains('party')) {
     body.classList.remove('party');
   } else {
     body.classList.add('party');
   }
-}
+};
 button.addEventListener('click', switchParty);
